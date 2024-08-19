40330e8ee516f248b8bcc9c10c61d6195b2a08f1
FSTDM <FSTDM@GMX.COM>
2024-08-19 13:32:30 -0700

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -4,11 +4,11 @@ import urllib.request
 
 print("[BOOTSTRAP] BEGIN")
 url = "https://api.github.com/repos/FSTDM/WBP/contents/index.py"
-print("\tdownloading")
+print("downloading")
 response = urllib.request.urlopen(url).read()
 jsonData = json.loads(response)
 filecontent = jsonData["content"]
 filedecoded = base64.b64decode(filecontent)
-print("\t executing")
+print(" executing")
 print("[BOOTSTRAP] END")
 exec(filedecoded)
