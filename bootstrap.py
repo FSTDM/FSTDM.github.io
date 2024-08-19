30b648321389efeb8d7857e0b8abc3e1830683a6
FSTDM <FSTDM@GMX.COM>
2024-08-19 13:39:05 -0700

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -1,3 +1,5 @@
+# https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28
+# HTTP request do not update the latest version, as quickly as using HTTP Github API
 import base64
 import json
 import urllib.request
