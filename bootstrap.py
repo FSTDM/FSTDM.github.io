db4e1ffaee7b5b62464eb0abae6600f49ed284e3
FSTDM <FSTDM@GMX.COM>
2024-08-19 13:33:28 -0700

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -2,13 +2,11 @@ import base64
 import json
 import urllib.request
 
-print("[BOOTSTRAP] BEGIN")
+print("[BOOTSTRAP]\ndownloading")
 url = "https://api.github.com/repos/FSTDM/WBP/contents/index.py"
-print("downloading")
 response = urllib.request.urlopen(url).read()
 jsonData = json.loads(response)
 filecontent = jsonData["content"]
 filedecoded = base64.b64decode(filecontent)
-print(" executing")
-print("[BOOTSTRAP] END")
+print(" executing\n[BOOTSTRAP] END\n")
 exec(filedecoded)
