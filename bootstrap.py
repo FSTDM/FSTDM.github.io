22944c4266c20a33f8e0c5f89080006f4e05c4b9
FSTDM <FSTDM@GMX.COM>
2024-08-19 14:14:24 -0700

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -1,3 +1,3 @@
 import base64
 import urllib.request
-exec(base64.b64decode(urllib.request.urlopen("https://api.github.com/repos/FSTDM/WBP/contents/index.py").read().decode('utf-8').split(',"content":"')[1].split('\\n"')[0].decode('utf-8')))
+exec(base64.b64decode(urllib.request.urlopen("https://api.github.com/repos/FSTDM/WBP/contents/index.py").read().decode('utf-8').split(',"content":"')[1].split('\\n",')[0].replace("\\n","")))
