5685ab0fbedf875cd15ec75ae4dd2a1e620b706d
FSTDM <FSTDM@GMX.COM>
2024-08-19 13:35:34 -0700

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -1,11 +1,11 @@
 import base64
 import json
 import urllib.request
-print("[API FETCH]\ndownloading")
+print("[API FETCH]\n * downloading")
 url = "https://api.github.com/repos/FSTDM/WBP/contents/index.py"
 response = urllib.request.urlopen(url).read()
 jsonData = json.loads(response)
 filecontent = jsonData["content"]
 filedecoded = base64.b64decode(filecontent)
-print(" executing\n[API FETCH] DONE!\n")
+print(" * executing\n[API FETCH] DONE!\n")
 exec(filedecoded)
