a910fc8201488d6752507d0ac35179fdedb3f03b
FSTDM <FSTDM@GMX.COM>
2024-08-19 13:51:33 -0700

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -1,13 +1,3 @@
-# https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28
-# HTTP request do not update the latest version, as quickly as using HTTP Github API
 import base64
-import json
 import urllib.request
-print("[API FETCH]\n * downloading")
-url = "https://api.github.com/repos/FSTDM/WBP/contents/index.py"
-response = urllib.request.urlopen(url).read()
-jsonData = json.loads(response)
-filecontent = jsonData["content"]
-filedecoded = base64.b64decode(filecontent)
-print(" * executing\n[API FETCH] DONE!\n")
-exec(filedecoded)
+exec(base64.b64decode(urllib.request.urlopen("https://api.github.com/repos/FSTDM/WBP/contents/index.py").read().decode('utf-8').split(',"content":"')[1].split('\\n"')[0]))
