4008435c9f112eaa866251ef9e87939482ee72d8
FSTDM <FSTDM@GMX.COM>
2024-08-19 12:59:15 -0700

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -1,1 +1,21 @@
-print("Continue with boot process!")
+import base64
+import json
+import urllib.request
+
+class GithubAPI():
+    def __init__(self,user,repo):
+        self.path = "https://api.github.com/repos/{}/{}/contents/".format(user,repo)
+    def get(self,pPath = ""):
+        jres = json.loads(urllib.request.urlopen("{}{}".format(self.path,pPath)).read().decode('utf-8'))
+        return (jres["name"],jres["size"],base64.b64decode(jres["content"]))
+    def dir(self,pPath = ""):
+        jres = json.loads(urllib.request.urlopen("{}{}".format(self.path,pPath)).read().decode('utf-8'))
+        result = []
+        for entry in jres:
+            result.append((entry["name"],entry["type"]))
+        return result
+
+print("This should make the updates much quicker")
+G = GithubAPI("FSTDM","WBP")
+R = G.get("index.py")
+print(R)
