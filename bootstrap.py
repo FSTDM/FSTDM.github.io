9b9fbd81fc6c201a7dfd63bf50b5c13988d68bb6
FSTDM <FSTDM@GMX.COM>
2024-08-19 13:19:34 -0700

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -2,18 +2,9 @@ import base64
 import json
 import urllib.request
 
-class GithubAPI():
-    def __init__(self,user,repo):
-        self.path = "https://api.github.com/repos/{}/{}/contents/".format(user,repo)
-    def get(self,pPath = ""):
-        jres = json.loads(urllib.request.urlopen("{}{}".format(self.path,pPath)).read().decode('utf-8'))
-        return (jres["name"],jres["size"],base64.b64decode(jres["content"]))
-    def dir(self,pPath = ""):
-        jres = json.loads(urllib.request.urlopen("{}{}".format(self.path,pPath)).read().decode('utf-8'))
-        result = []
-        for entry in jres:
-            result.append((entry["name"],entry["type"]))
-        return result
-
-exec(GithubAPI("FSTDM","WBP").get("index.py").[2].decode('utf-8'))
-
+url = "https://api.github.com/repos/FSTDM/WBP/contents/index.py"
+response = urllib.request.urlopen(url).read()
+jsonData = json.loads(response)
+filecontent = jsonData["content"]
+filedecoded = base64.b64decode(filecontent)
+exec(filedecoded)
