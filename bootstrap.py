8ceddc11ac29995c577bf51bdff513fe6014bb2a
FSTDM <FSTDM@GMX.COM>
2024-08-20 11:36:50 -0700

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -2,7 +2,7 @@
 #Github API updates quicker than HTTP Request
 import base64
 import urllib.request
-url = "https://api.github.com/repos/FSTDM/WBP/contents/index.py"
+url = "https://api.github.com/repos/FSTDM/WBP/contents/autoexec.py"
 response = urllib.request.urlopen(url).read().decode('utf-8')
 b64ContentWithNewLines = response.split(',"content":"')[1].split('\\n",')[0]
 b64Content = b64ContentWithNewLines.replace("\\n","")
